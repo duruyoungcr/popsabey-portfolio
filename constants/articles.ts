4f2ad4d5095bcdae4505a4836ece51f9dfbdae06
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-06 23:34:47 +0100

    New: Created constants for reviews and articles

---
+++ constants/articles.ts
@@ -0,0 +1,13 @@
+
+const articles = [
+    {
+        title: "Designing UI Dashboards: The Beginner’s Approach",
+        link: '/'
+    },
+    {
+        title: "Design System, the basics",
+        link: '/'
+    }
+];
+
+export default articles
---
+++ constants/reviews.ts
@@ -0,0 +1,18 @@
+
+export const baseImagePath = '/assets/img/reviews/'
+
+const reviews = [
+    {
+        text: "Abiodun is a driven individual, a man who's sole desire is to do good work. It is always a pleasure working with him and learning new things from him.",
+        name: 'Mudia imasuen',
+        position: 'Lead Designer, Bloc',
+    },
+    {
+        text: "Abiodun is one of the best and most driven designer I have worked with. His passion for quality work is unmatched. He is a good communicator, team-player.",
+        name: 'Adolphus chris',
+        position: 'Senior Designer, Sqaudpal',
+    }
+];
+
+
+export default reviews
