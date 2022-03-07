8b07534f59607d9c316a5912ac510bf130a4b9e2
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-07 14:24:04 +0100

    Update: slight modifications

--- components/home/Projects/index.tsx
+++ components/home/Projects/index.tsx
@@ -1,10 +1,24 @@
+import projects from '../../../constants/projects'
 import CustomHeading from '../../general/CustomHeading'
+import Project from '../Project'
 import styles from './Projects.module.css'
 
 const Projects = () => {
     return (
-        <section className={styles.container}>
+        <section className={styles.container} id='projects'>
             <CustomHeading text='some of my work' fontSize='text-4xl' marginBottom='mb-10' />
+            <div className={styles.list}>
+                {projects.map(project => (
+                    <Project
+                        title={project.title}
+                        desc={project.desc}
+                        slug={project.slug}
+                        key={project.title}
+                        live={project.live}
+                        blurDataURL={project.blurDataURL}
+                    />
+                ))}
+            </div>
         </section>
     )
 }
