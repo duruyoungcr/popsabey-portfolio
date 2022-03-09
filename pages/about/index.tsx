b725b8643fb6d85487e6b3915eafe710ed4d7361
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-09 08:52:43 +0100

    Update: Replaced Head with SEO component

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useContext, useEffect } from 'react'
+import ExperienceList from '../../components/about/ExperienceList'
+import Hero from '../../components/about/Hero'
+import Hobbies from '../../components/about/Hobbies'
+import SkillSet from '../../components/about/SkillSet'
 import Layout from '../../components/layout/Layout'
+import SEO from '../../components/layout/SEO'
 import Loader from '../../components/Loader'
 import { LoadingContext } from '../../context/LoadingContext'
 import styles from './About.module.css'
@@ -23,13 +28,12 @@ const About: NextPage = (props) => {
     }
     return (
         <>
-            <Head>
-                <title>About</title>
-                <meta name="description" content="About" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+            <SEO title="About | Popsabey Portfolio" />
             <Layout>
-                About Page
+                <Hero />
+                <SkillSet />
+                <ExperienceList />
+                <Hobbies />
             </Layout>
         </>
     )
