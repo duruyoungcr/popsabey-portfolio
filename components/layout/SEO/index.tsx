4d1078a5d82e919065840146af4b4c177b52c952
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-09 09:33:38 +0100

    Update: Added favicon links to SEO component

--- components/layout/SEO/index.tsx
+++ components/layout/SEO/index.tsx
@@ -12,6 +12,10 @@ const SEO = ({ title, description }: SEOPropsTypes) => {
             <title>{title}</title>
             <meta name="description" content={description || 'Portfolio Page of Popsabey'} />
             <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
         </Head>
     )
 }
