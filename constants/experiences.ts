00f5f6884f7973e726c858547d77518868b89343
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-09 08:50:42 +0100

    New: Added experiences and skills constants

---
+++ constants/experiences.ts
@@ -0,0 +1,36 @@
+const experiences = [
+    {
+        timeline: "May 2021 - Present",
+        company: "FoodYours (Abuja, Nigeria)",
+        position: "PRODUCT DESIGNER",
+        type: "PRESENT",
+        future: false,
+        recent: false,
+    },
+    {
+        timeline: "Jan 2021 - May 2021(Contract)",
+        company: "Chidalu Software (Ontario, Canada)",
+        position: "PRODUCT DESIGNER",
+        type: "Past",
+        future: false,
+        recent: true,
+    },
+    {
+        timeline: "Aug 2020 - Dec 2020",
+        company: "Kufuta Boom (Luanda, Angola)",
+        position: "Kufuta Boom (Luanda, Angola)",
+        type: "Past",
+        future: false,
+        recent: false,
+    },
+    {
+        timeline: "",
+        company: "",
+        position: "",
+        type: "Future",
+        future: true,
+        recent: false,
+    },
+];
+
+export default experiences;
---
+++ constants/skills.ts
@@ -0,0 +1,16 @@
+const skills = [
+    { 
+        title: "Product Design",
+        description: "I strive to create product experience and strategies that resonates with customer and business requirements.",
+    },
+    { 
+        title: "Product Management",
+        description: "Product development can turn to total chaos without a set out goal, bringing product direction with efficiency is my where i come in.",
+    },
+    { 
+        title: "Brand Design",
+        description: "There are tons of brands out there and new ones are created everyday. How a brand stands tells it’s story distinguish-ably is what i do.",
+    }
+];
+
+export default skills
