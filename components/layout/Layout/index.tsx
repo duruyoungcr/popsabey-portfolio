336d934bc1ba5ab2dead093f7d2f43f5cd2ff9e7
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-18 09:34:58 +0100

    Update: restructred for layout to handle display of loader

--- components/layout/Layout/index.tsx
+++ components/layout/Layout/index.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { LoadingContext } from '../../../context/LoadingContext'
+import Loader from '../../Loader'
 import Footer from '../Footer'
 import Header from '../Header'
 import styles from './Layout.module.css'
@@ -11,14 +13,22 @@ interface ChildrenProps {
 
 const Layout = ({ children }: ChildrenProps) => {
     const [path, setPath] = useState<string>('')
+    const { loading } = useContext(LoadingContext)
     const router = useRouter();
     useEffect(() => {
         setPath(router.pathname);
     }, [router.route])
+    if (loading) {
+        return (
+            <Loader />
+        )
+    }
     return (
         <div className={styles.container}>
             <Header path={path} />
-            <main className={styles.main}>{children}</main>
+            <main className={styles.main}>
+                {children}
+            </main>
             <Footer />
         </div>
     )
