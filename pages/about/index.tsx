bd9b3fe46b88374a170ebe0918937ad6b85c4c06
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-18 09:34:26 +0100

    Update: cleaned up page, removed unused imports

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,31 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import { useContext, useEffect } from 'react'
 import ExperienceList from '../../components/about/ExperienceList'
 import Hero from '../../components/about/Hero'
 import Hobbies from '../../components/about/Hobbies'
 import SkillSet from '../../components/about/SkillSet'
 import Layout from '../../components/layout/Layout'
 import SEO from '../../components/layout/SEO'
-import Loader from '../../components/Loader'
-import { LoadingContext } from '../../context/LoadingContext'
-import styles from './About.module.css'
 
 const About: NextPage = (props) => {
-    const { loading, setLoading } = useContext(LoadingContext)
-
-    useEffect(() => {
-        setLoading(true)
-        const subscribe = setTimeout(() => {
-            setLoading(false)
-        }, 5000);
-        return () => {
-            subscribe
-        }
-    }, [])
-    if (loading) {
-        return <Loader />
-    }
     return (
         <>
             <SEO title="About | Popsabey Portfolio" />
