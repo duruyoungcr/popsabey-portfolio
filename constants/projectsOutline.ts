fa828030f7b679434c00adca1ef057e13b984d46
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-18 09:15:37 +0100

    New: Created projects outline constant file

---
+++ constants/projectsOutline.ts
@@ -0,0 +1,43 @@
+
+const insuranceApp = {
+    title: "REDEFINING THE INSURANCE SYSTEM",
+    description: "Data from National Bereau of Statistics(Nigeria) shows insurance adoption is at an infancy stage in Nigeria, there seems to be a critically low adoption of insurance services especially among millenials and GenZs in the country.",
+    year: "2020",
+    role: "Product Designer",
+    industry: "Fintech",
+    duration: "5+ weeks",
+    platform: "Mobile App",
+}
+
+const cryptoApp = {
+    title: "A SIMPLIFIED CRYPTO TRADING PLATFORM",
+    description: "Thousands of Africans still have little idea how to buy, sell, trade crypto, tons of complaints about the complexity of crypto platforms. Taking this first hand data, i designed a solution, an app simple and prescise to use for this purpose..",
+    year: "2020",
+    role: "Product Designer",
+    industry: "Fintech/web3",
+    duration: "",
+    platform: "Andriod, iOS",
+}
+
+const designSystem = {
+    title: "REZZARS’ DESIGN SYSTEM: DESIGN AND MANAGEMENT",
+    description: "The Rezzar design team just started out, growing from one(1) designer to three(3) in a short time and with the view of future expansion due to intensity of work on the teams roadmap. There is need to define and determine the design language and thr collaborative platform for the team and the engineering team.",
+    year: "2022",
+    role: "Product Designer",
+    industry: " General",
+    duration: "",
+    platform: "",
+}
+
+const flightApp = {
+    title: "FLIGHT BOOKING AND RESERVATION",
+    description: "Traveling has been in high demand post covid, people are aiming to “see the world” again after such a long time confined in a place; however the offline flight booking system in Africa doesn’t seem to suffice for the airline’s travelers surge in demand anymore",
+    year: "2021",
+    role: "Product Designer",
+    industry: "Aviation",
+    duration: "8 weeks",
+    platform: "Andriod, iOS",
+}
+
+
+export { insuranceApp, cryptoApp, flightApp, designSystem }
