33db49eb50e12f124b289879f3288c339880b23c
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-18 09:18:27 +0100

    Update: minor style change

--- components/general/BadgeHeading/index.tsx
+++ components/general/BadgeHeading/index.tsx
@@ -1,8 +1,8 @@
 
 const BadgeHeading = ({ text }: { text: string }) => {
     return (
-        <div className='border border-blue-light rounded-lg py-3.5 px-6 w-[225px] bg-blue-darkest mb-10'>
-            <p className='text-lg uppercase text-white-darkest'>{text}</p>
+        <div className='border border-blue-light rounded-lg py-3.5 px-6 min-w-[225px] max-w-max bg-blue-darkest mb-10 text-lg uppercase text-white-darkest'>
+            {text}
         </div>
     )
 }
