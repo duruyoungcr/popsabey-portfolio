f07ecfb071198b02485ac8e17a3e95e54dbf8917
Duru-young Raymond <duruyoungcr@gmail.com>
2022-04-17 03:38:27 +0100

    Update: removal error warning for semicolon

--- components/about/Skill/index.tsx
+++ components/about/Skill/index.tsx
@@ -3,7 +3,7 @@ const Skill = ({ title, description }: { title: string, description: string }) =
     return (
         <div
             style={{
-                background: 'linear-gradient(180deg, #030732 0%, #010424 100%);',
+                background: 'linear-gradient(180deg, #030732 0%, #010424 100%)',
             }}
             className='border border-black-light rounded-2xl min-h-[235px] px-7 py-6 transition-all duration-500 ease-in-out hover:-translate-y-6 hover:shadow hover:shadow-blue-light'
         >
