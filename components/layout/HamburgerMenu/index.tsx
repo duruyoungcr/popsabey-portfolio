78fdd0ed85a9c3db9f869f192a61b3fdd72fd36a
Duru-young Raymond <duruyoungcr@gmail.com>
2022-04-24 19:20:12 +0100

    Update: responsive and animation of mobile nav menu

--- components/layout/HamburgerMenu/index.tsx
+++ components/layout/HamburgerMenu/index.tsx
@@ -3,9 +3,9 @@ import styles from './HamburgerMenu.module.css'
 const HamburgerMenu = ({ toggle, handleToggle }: { toggle: boolean, handleToggle: () => void }) => {
     return (
         <div role="button" onClick={handleToggle} className={styles.nav}>
-            <span className={`${styles.line} ${toggle ? '-rotate-45 translate-x-2 translate-y-2.5 w-12' : 'w-6'}`}></span>
+            <span className={`${styles.line} ${toggle ? '-rotate-45 translate-y-2.5 w-8' : 'w-6'}`}></span>
             <span className={`${styles.line} ${toggle ? 'opacity-0' : ''}`}></span>
-            <span className={`${styles.line} ${toggle ? 'rotate-45 translate-x-2 -translate-y-2.5 w-12' : 'w-6'}`}></span>
+            <span className={`${styles.line} ${toggle ? 'rotate-45  -translate-y-2.5 w-8' : 'w-6'}`}></span>
         </div>
     )
 }
--- components/layout/MobileMenu/index.tsx
+++ components/layout/MobileMenu/index.tsx
@@ -2,38 +2,97 @@ import Link from 'next/link'
 import socials from '../../../constants/socials'
 import SocialLink from '../../general/SocialLink'
 import styles from './MobileMenu.module.css'
+import { motion } from "framer-motion";
+
+const variants = {
+    visible: (i: number) => ({
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: i * 0.3,
+            easing: "easeInOut"
+        },
+    }),
+    hidden: { opacity: 0, y: 50 },
+    exit: {
+        opacity: 0, y: 50
+    }
+}
 
 const MobileMenu = ({ path, handleToggle }: { path: string, handleToggle: () => void }) => {
     return (
-        <div className={styles.container}>
-            <div className={styles.nav}>
-                <Link href={'/about'}>
-                    <a className={`${styles.navLink} ${path === '/about' && 'text-white-light'}`}>
+        <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            className={styles.container}
+        >
+            <motion.div
+                className={styles.nav}
+                initial={{ x: 100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+            >
+                <Link href={'/about'} >
+                    <motion.a
+                        custom={1}
+                        variants={variants}
+                        animate='visible'
+                        initial='hidden'
+                        className={`${styles.navLink} ${path === '/about' && 'text-white-light'}`}
+                    >
                         About
-                    </a>
+                    </motion.a>
                 </Link>
                 <Link href={'/#projects'}>
-                    <a onClick={handleToggle} className={`${styles.navLink} ${path === '/projects' && 'text-white-light'}`}>
+                    <motion.a
+                        custom={2}
+                        variants={variants}
+                        animate='visible'
+                        initial='hidden'
+                        onClick={handleToggle}
+                        className={`${styles.navLink} ${path === '/projects' && 'text-white-light'}`}
+                    >
                         Projects
-                    </a>
+                    </motion.a>
                 </Link>
                 <Link href={'/uis'}>
-                    <a className={`${styles.navLink} ${path === '/uis' && 'text-white-light'}`}>
+                    <motion.a
+                        custom={3}
+                        variants={variants}
+                        animate='visible'
+                        initial='hidden'
+                        className={`${styles.navLink} ${path === '/uis' && 'text-white-light'}`}
+                    >
                         UIs
-                    </a>
+                    </motion.a>
                 </Link>
                 <Link href={`${path}#contact`}>
-                    <a onClick={handleToggle} className={`${styles.navLink} ${path === '/contact' && 'text-white-light'}`}>
+                    <motion.a
+                        custom={4}
+                        variants={variants}
+                        animate='visible'
+                        initial='hidden'
+                        exit='exit'
+                        onClick={handleToggle}
+                        className={`${styles.navLink} ${path === '/contact' && 'text-white-light'}`}
+                    >
                         Contact Me
-                    </a>
+                    </motion.a>
                 </Link>
-            </div>
+            </motion.div>
             <div className={styles.socials}>
                 {socials.map((social, index) => (
-                    <SocialLink href={social.href} key={index} name={social.name} width={social.width} height={social.height} />
+                    <motion.div
+                        custom={6 + index}
+                        variants={variants}
+                        animate='visible'
+                        initial='hidden'
+                        exit='exit'
+                    >
+                        <SocialLink href={social.href} key={index} name={social.name} width={social.width} height={social.height} />
+                    </motion.div>
                 ))}
             </div>
-        </div>
+        </motion.div>
     )
 }
 
