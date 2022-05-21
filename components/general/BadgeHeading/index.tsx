6f9a489916e4ed68c2ea12b914b4c4d5884bca10
Raymond <duruyoungcr@gmail.com>
2022-05-21 14:33:13 +0100

    Bug: Fix for overflow

--- components/general/BadgeHeading/index.tsx
+++ components/general/BadgeHeading/index.tsx
@@ -1,7 +1,7 @@
 
 const BadgeHeading = ({ text }: { text: string }) => {
     return (
-        <div data-aos='fade-up' className='border border-blue-light rounded-lg py-3 sm:py-3.5 px-4 sm:px-6 min-w-max max-w-max bg-blue-darkest mb-4 sm:mb-10 sm:text-lg uppercase text-white-darkest'>
+        <div data-aos='fade-up' className='border border-blue-light rounded-lg py-3 sm:py-3.5 px-4 sm:px-6 sm:min-w-[225px] w-max bg-blue-darkest mb-4 sm:mb-10 sm:text-lg uppercase text-white-darkest'>
             {text}
         </div>
     )
