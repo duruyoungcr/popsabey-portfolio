f2e086d07f6fab684368f6e1c14606614ba460a2
Popsabey <59439480+Popsabey@users.noreply.github.com>
2022-06-22 01:46:44 +0100

    Update articles.ts

--- constants/articles.ts
+++ constants/articles.ts
@@ -7,7 +7,7 @@ const articles = [
     {
         title: "Design System, the basics",
         link: 'https://medium.com/@popsabey.ux/design-system-the-basics-b27d5043fa81?source=user_profile---------0----------------------------'
-    }
+    },
      {
         title: "Design Tokens: What are those?",
         link: 'https://medium.com/@popsabey.ux/design-tokens-what-are-those-cca18c7ea72e'
