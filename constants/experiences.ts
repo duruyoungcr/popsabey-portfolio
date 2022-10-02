0758ec9e65d6b36c7ff671718a7944286ee5148a
Popsabey <59439480+Popsabey@users.noreply.github.com>
2022-10-02 23:54:24 +0100

    Update experiences.ts

--- constants/experiences.ts
+++ constants/experiences.ts
@@ -1,14 +1,14 @@
 const experiences = [
        {
         timeline: "March 2022 - Present",
-        company: "DressmakerGQ (Nigeria)",
+        company: "DressmakerHQ (Remote)",
         position: "SENIOR PRODUCT DESIGNER",
         type: "PRESENT",
         future: false,
         recent: false,
     },
     {
-        timeline: "May 2021 - Present",
+        timeline: "May 2021 - Jul 2022",
         company: "FoodYours (Abuja, Nigeria)",
         position: "PRODUCT DESIGNER",
         type: "Past",
@@ -24,7 +24,7 @@ const experiences = [
         recent: false,
     },
     {
-        timeline: "Aug 2020 - Dec 2020",
+        timeline: "Aug 2020 - Dec 2020(Contract",
         company: "Kufuta Boom (Luanda, Angola)",
         position: "PRODUCT DESIGNER",
         type: "Past",
